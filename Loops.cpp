11685a269274c13d22fcca6d175a7eee80d3c209
drshivago <moessner2015@gmail.com>
2023-01-03 19:59:32 -0500

    First commit

---
+++ Loops.cpp
@@ -0,0 +1,16 @@
+//
+//  Loops.cpp
+//  
+//
+//  Created by Steven on 1/3/23.
+//
+
+#include <stdio.h>
+
+int main(){
+    cout << "Welcome to loops!";
+    
+    for(int i(0); i < 20; i++){
+        
+    }
+}
